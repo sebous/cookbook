45fee62b6b2273ed8c167eb9ab427547f141500c
sebous <sebous@gmail.com>
2022-05-17 12:24:52 +0200

    chore: cleanup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { trpc } from "@/utils/trpc";
 import type { NextPage } from "next";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
   const recipes = trpc.useQuery(["all-recipes"]);
@@ -9,9 +8,8 @@ const Home: NextPage = () => {
     return <div>Loading...</div>;
   }
 
-  console.log(recipes.data);
   return (
-    <div className={styles.container}>
+    <div>
       {recipes.data.map((r) => (
         <p key={r.id}>
           {r.id} {r.name}
